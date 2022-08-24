94960e3174f75715025cae0aaa2b4f488399ad71
tigerbluejay <josemariairiarte@gmail.com>
2022-08-24 15:20:18 -0300

    first commit

---
+++ ContosoPizzaEFDemoDBFirstSolution/ContosoPizzaEFDemoDBFirst/Program.cs
@@ -0,0 +1,10 @@
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaEFDemoPart1Context context = new ContosoPizzaEFDemoPart1Context();
+
+foreach (Customer c in context.Customers)
+{
+    // nothing will print if there is no data in the database table
+    Console.WriteLine($"Name: {c.FirstLast}");
+}
